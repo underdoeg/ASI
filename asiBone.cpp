857b060136b06772a97784239e6aaf33aeec7ef2
Philip Whitfield <public@underdoeg.com>
2010-09-01 17:56:00 +0200

    initial bones support

---
+++ asiBone.cpp
@@ -0,0 +1,19 @@
+/*
+ *  asiBone.cpp
+ *  pclGame
+ *
+ *  Created by Philip Whitfield on 9/1/10.
+ *  Copyright 2010 undef.ch. All rights reserved.
+ *
+ */
+
+#include "asiBone.h"
+
+
+asiBone::asiBone(){
+
+};
+
+asiBone::~asiBone(){
+
+};
---
+++ asiBone.h
@@ -0,0 +1,22 @@
+/*
+ *  asiBone.h
+ *  pclGame
+ *
+ *  Created by Philip Whitfield on 9/1/10.
+ *  Copyright 2010 undef.ch. All rights reserved.
+ *
+ */
+
+#ifndef _CLASS_asiBone_
+#define _CLASS_asiBone_
+#include "asiTypes.h"
+
+class asiBone: public asiObjectBase{
+public:
+      asiBone();
+      ~asiBone();      
+protected:
+private:
+};
+
+#endif
